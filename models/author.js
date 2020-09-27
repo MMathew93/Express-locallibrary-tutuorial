3ee0104ce0796b39de610d73353d2ef275baab0e
lyndexer <mckinney.m93@gmail.com>
2020-09-27 11:03:45 -0700

    First version of app moved to github

---
+++ models/author.js
@@ -0,0 +1,48 @@
+let mongoose = require('mongoose');
+let moment = require('moment');
+
+let Schema = mongoose.Schema;
+
+let AuthorSchema = new Schema(
+    {
+        first_name: {type: String, required: true, maxlength: 100},
+        family_name: {type: String, required: true, maxlength: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+);
+
+//Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function() {
+    //to avoid errors in cases where an author does not have either a family name or first name
+    //We want to make sure we handle the exception by returning an empty string for that case
+
+    let fullname = '';
+    if(this.first_name && this.family_name) {
+        fullname = this.family_name + ', ' + this.first_name
+    }
+    if(!this.first_name || !this.family_name) {
+        fullname = '';
+    }
+
+    return fullname;
+});
+
+//Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function() {
+    return (this.date_of_birth ? moment(this.date_of_birth).format('YYYY/DD/MM') : '').toString() + ' - ' + (this.date_of_death ? moment(this.date_of_death).format('YYYY/DD/MM') : '').toString();
+});
+
+//Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function() {
+    return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
